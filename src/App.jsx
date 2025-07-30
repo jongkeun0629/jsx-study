dd96b873358d03c18d60bbceaaa4cb0cfe17efd4
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-07-30 11:42:08 +0900

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
 import "./App.css";
 import UserCard from "./components/UserCard";
 
+const userData = [
+  {
+    id: 1,
+    name: "jongkeun",
+    job: "Prgrammer",
+    imageSrc: "avatar.webp",
+  },
+  {
+    id: 2,
+    name: "이종근",
+    job: "돈 많은 백수",
+    imageSrc: "avatar2.webp",
+  },
+  {
+    id: 3,
+    name: "ㅎㅇ",
+    job: "작가",
+    imageSrc: "avatar3.jpeg",
+  },
+];
+
 const App = () => {
   const name = "jongkeun";
 
   return (
     <>
-      <div className="card">
-        <img src="/avatar.webp" alt="avatar"></img>
-        <h2>jongkeun</h2>
-        <h2>Programmer</h2>
-      </div>
-      <UserCard />
+      {userData.map((value) => (
+        <UserCard
+          key={value.id}
+          name={value.name}
+          job={value.job}
+          imageSrc={value.imageSrc}
+        />
+      ))}
     </>
   );
 };
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,5 +1,11 @@
-const UserCard = () => {
-  return <div>UserCard</div>;
+const UserCard = ({ name, job, imageSrc }) => {
+  return (
+    <div className="card">
+      <img src={imageSrc} alt={name} />
+      <h2>{name}</h2>
+      <h2>{job}</h2>
+    </div>
+  );
 };
 
 export default UserCard;
