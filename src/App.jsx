128c925925f42fa35e260a7d43d766095e91b299
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-07-30 10:32:16 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+const App = () => {
+  const name = "jongkeun";
+
+  return (
+    <>
+      <div className="card">
+        <img src="/avatar.webp" alt="avatar"></img>
+        <h2>jongkeun</h2>
+        <h2>Programmer</h2>
+      </div>
+    </>
+  );
+};
+
+export default App;
